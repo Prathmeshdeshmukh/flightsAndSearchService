4295ec17ae45070be6496e1df86cdbc60027edeb
Prathmeshdeshmukh <87267186+Prathmeshdeshmukh@users.noreply.github.com>
2023-02-12 12:57:20 +0530

    added compareTime feature

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,17 +1,22 @@
 const { FlightRepository , AirplaneRepository } = require('../repository/index');
+const { compareTime } = require('../utils/helper');
+
 
 class FlightService{
     constructor(){
-        this.airPlaneRepository = new AirplaneRepository();   
+        this.airplaneRepository = new AirplaneRepository();   
         this.flightRepository = new FlightRepository();
     }
 
     async createFlight(data){
         try {
             // const airPlaneRepository = new AirplaneRepository();
+            if(!compareTime(data.arrivalTime , data.departureTime)){
+                throw {error : 'arrival time cannot be less than departure time'}
+            }
              const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
              const flight = await this.flightRepository.createFlight({
-                ...data , totalSeats : airplane.capacity
+                ...data , totalSeats:airplane.capacity
              })
              return flight;
         } catch (error) {
